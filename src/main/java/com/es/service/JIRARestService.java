9f183e30708a287f24db5f413ff4a6baac75da89
mallika Gopal <mallika.gopal@mitre10.co.nz>
2023-10-27 10:33:00 +0530

    Formatted code for getting Projects from JIRA.

--- src/main/java/com/es/service/JIRARestService.java
+++ src/main/java/com/es/service/JIRARestService.java
@@ -25,8 +25,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
-
 @Service
 public class JIRARestService {
 
@@ -34,93 +32,90 @@ public class JIRARestService {
 	private String jira_username;
 
 	@Value("${jira.token}")
-	private String 	jira_token;
+	private String jira_token;
 
 	@Value("${jira.base.url}")
-	private String 	jira_base_url;
+	private String jira_base_url;
 
 	@Value("${jira.get.project.endpoint}")
 	private String jira_get_project;
 
-
 	@Autowired
 	ClientCredentialsService clientCredentialsService;
-	
+
 	public List<ProjectInfoDto> getAllProjects(int clientId) {
-		
+
 		ClientCredentials clientCredentials = new ClientCredentials();
 		clientCredentials = clientCredentialsService.getClientCredentials(clientId);
-		
-		if(clientCredentials != null) {
-			
-			 	RestTemplate restTemplate = new RestTemplate();
-			    HttpHeaders headers = new HttpHeaders();
-			    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-			    headers.setBasicAuth(clientCredentials.getJiraUserName(), clientCredentials.getToken());
-			    HttpEntity<String> entity = new HttpEntity<>(headers);
 
-			    ResponseEntity<String> responseEntity = restTemplate.exchange(jira_base_url + jira_get_project, HttpMethod.GET, entity, String.class);
-			    String jsonResponse = responseEntity.getBody();
-			    JsonParser jsonParser = new JsonParser();
-			    JsonObject jsonObject = jsonParser.parse(jsonResponse).getAsJsonObject();
+		if (clientCredentials != null) {
 
-			    JsonArray values = jsonObject.getAsJsonArray("values");
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+			headers.setBasicAuth(clientCredentials.getJiraUserName(), clientCredentials.getToken());
+			HttpEntity<String> entity = new HttpEntity<>(headers);
 
-			    List<ProjectInfoDto> projectInfoList = new ArrayList<>();
+			ResponseEntity<String> responseEntity = restTemplate.exchange(jira_base_url + jira_get_project,
+					HttpMethod.GET, entity, String.class);
+			String jsonResponse = responseEntity.getBody();
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObject = jsonParser.parse(jsonResponse).getAsJsonObject();
 
-			    for (int i = 0; i < values.size(); i++) {
-			        JsonObject projectObject = values.get(i).getAsJsonObject();
-			        int projectId = projectObject.get("id").getAsInt();
-			        JsonObject location = projectObject.getAsJsonObject("location");
-			        String projectName = location.get("projectName").getAsString();
+			JsonArray values = jsonObject.getAsJsonArray("values");
 
-			        ProjectInfoDto projectInfo = new ProjectInfoDto(projectId, projectName);
-			        projectInfoList.add(projectInfo);
-			        
-			    }
-			        
-			        return projectInfoList;
-		
-		}
-		else {
-			
+			List<ProjectInfoDto> projectInfoList = new ArrayList<>();
+
+			for (int i = 0; i < values.size(); i++) {
+				JsonObject projectObject = values.get(i).getAsJsonObject();
+				int projectId = projectObject.get("id").getAsInt();
+				JsonObject location = projectObject.getAsJsonObject("location");
+				String projectName = location.get("projectName").getAsString();
+
+				ProjectInfoDto projectInfo = new ProjectInfoDto(projectId, projectName);
+				projectInfoList.add(projectInfo);
+
+			}
+
+			return projectInfoList;
+
+		} else {
+
 			return null;
-			
+
 		}
-}
-	
-	public List<SprintInfoDto> getAllSprintsByProjectId(int projectId) {
-	    RestTemplate restTemplate = new RestTemplate();
-	    
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-	    headers.setBasicAuth(jira_username, jira_token);
-	    HttpEntity<String> entity = new HttpEntity<>(headers);
-
-	   String jiraSprintEndpoint = jira_base_url + jira_get_project + projectId + "/sprint";
-
-	    ResponseEntity<String> responseEntity = restTemplate.exchange(jiraSprintEndpoint, HttpMethod.GET, entity, String.class);
-	    String jsonResponse = responseEntity.getBody();
-	    JsonParser jsonParser = new JsonParser();
-	    JsonObject jsonObject = jsonParser.parse(jsonResponse).getAsJsonObject();
-
-	    JsonArray values = jsonObject.getAsJsonArray("values");
-
-	    List<SprintInfoDto> sprintInfoList = new ArrayList<>();
-
-	    for (int i = 0; i < values.size(); i++) {
-	        JsonObject sprintObject = values.get(i).getAsJsonObject();
-	        int sprintId = sprintObject.get("id").getAsInt();
-	        String sprintName = sprintObject.get("name").getAsString();
-
-	        SprintInfoDto sprintInfo = new SprintInfoDto(sprintId, sprintName);
-	        sprintInfoList.add(sprintInfo);
-	    }
-
-	    return sprintInfoList;
 	}
-	
-	
 
-	
+	public List<SprintInfoDto> getAllSprintsByProjectId(int projectId) {
+		RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		headers.setBasicAuth(jira_username, jira_token);
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+
+		String jiraSprintEndpoint = jira_base_url + jira_get_project + projectId + "/sprint";
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(jiraSprintEndpoint, HttpMethod.GET, entity,
+				String.class);
+		String jsonResponse = responseEntity.getBody();
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jsonObject = jsonParser.parse(jsonResponse).getAsJsonObject();
+
+		JsonArray values = jsonObject.getAsJsonArray("values");
+
+		List<SprintInfoDto> sprintInfoList = new ArrayList<>();
+
+		for (int i = 0; i < values.size(); i++) {
+			JsonObject sprintObject = values.get(i).getAsJsonObject();
+			int sprintId = sprintObject.get("id").getAsInt();
+			String sprintName = sprintObject.get("name").getAsString();
+
+			SprintInfoDto sprintInfo = new SprintInfoDto(sprintId, sprintName);
+			sprintInfoList.add(sprintInfo);
+		}
+
+		return sprintInfoList;
+	}
+
 }
